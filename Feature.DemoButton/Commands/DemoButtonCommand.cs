63684b3ecec81e1658c7b45d26bac7e92975022e
Phil Busch <phil.busch@layeronemedia.com>
2020-01-20 17:05:19 -0600

    DemoButtonCommand implementation

---
+++ Feature.DemoButton/Commands/DemoButtonCommand.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Shell.Framework.Commands;
+using System;
+
+namespace Feature.DemoButton.Commands
+{
+    public class DemoButtonCommand : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Sitecore.Context.ClientPage.ClientResponse.Alert("This demo is working!");
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            if (context.Items[0].Name.Equals("Disabled", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return CommandState.Disabled;
+            }
+
+            return base.QueryState(context);
+        }
+    }
+}
